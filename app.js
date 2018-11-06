94bf85a7504143bdc4e00c86a5a3c05736e3ac21
phangs <wlpineda@gmail.com>
2018-11-06 18:27:09 +0800

    4th update

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(bodyParser.json());
 
 routes(app);
 
-var server = app.listen(3000, function () {
-    console.log("app running on port.", server.address().port);
+var server = app.listen(8080, function () {
+    // console.log("app running on port.", server.address().port);
 });
